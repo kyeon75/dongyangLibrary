b3c55a4b3ec4b57b254a0fce06fbbae0def9912c
kyw <rayw1296@naver.com>
2022-11-26 19:21:23 +0900

    장바구니, 결제페이지 이동 왜 안됨 --?

---
+++ src/main/java/com/user/buy/BuyFrontController.java
@@ -0,0 +1,60 @@
+package com.user.buy;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.view.book.Action;
+import com.view.book.BestBookListAction;
+import com.view.book.DiscountBookListAction;
+import com.view.book.HotBookListAction;
+import com.view.user.MyInfoAction;
+import com.view.user.SignUpAction;
+
+
+@WebServlet("/buy/*")
+public class BuyFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+	
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String page = requestURI.substring(contextPath.length());
+		page = page.substring(5);
+		
+		Action action = null;
+		String viewPage = null;
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		if (page.equals("/bookkart")) {
+			action = new BookkartAction();
+			viewPage = action.execute(request, response);
+			
+		} else if (page.equals("/payment")) {
+			action = new PaymentAction();
+			viewPage = action.execute(request, response);
+		} 
+		
+		request.setAttribute("page", viewPage);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
